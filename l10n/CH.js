cd80f169a0e75c2a652d3f1d439e942820c22efe
ytzyangting <ytzyangting@126.com>
2016-05-23 13:28:37 +0800

    首次提交

---
+++ l10n/CH.js
@@ -0,0 +1,78 @@
+{
+
+"header" : {
+  "navbar" : {
+    "UPLOAD" : "上传",
+    "new" : {
+      "NEW" : "最新消息",
+      "PROJECT" : "项目",
+      "TASK" : "任务",
+      "USER" : "人员",
+      "EMAIL" : "邮件"
+    },
+    "NOTIFICATIONS" : "通知"
+  }
+},
+"aside" : {
+  "nav" : {
+    "HEADER" : "导航",
+    "DASHBOARD" : "仪表板",
+    "CALENDAR" : "日历",
+    "EMAIL" : "邮件",
+    "WIDGETS" : "窗口小部件",
+    "components" : {
+      "COMPONENTS" : "组件",
+      "ui_kits" : {
+        "UI_KITS" : "UI工具",
+        "BUTTONS" : "按钮",
+        "ICONS" : "图标",
+        "GRID" : "网格",
+        "BOOTSTRAP" : "Bootstrap",
+        "SORTABLE" : "排序",
+        "PORTLET" : "小窗口",
+        "TIMELINE" : "时间轴",
+        "VECTOR_MAP" : "矢量地图"
+      },
+      "Layout" : {
+        "LAYOUT" : "布局",
+        "APPLICATION" : "应用",
+        "FULL_WIDTH" : "全宽度",
+        "MOBILE" : "手机"
+      },
+      "table" : {
+        "TABLE" : "表格",
+        "TABLE_STATIC" : "Table static",
+        "DATATABLE" : "Datatable",
+        "FOOTABLE" : "Footable"
+      },
+      "form" : {
+        "FORM" : "表单",
+        "FORM_ELEMENTS" : "表单元素",
+        "FORM_VALIDATION" : "表单验证",
+        "FORM_WIZARD" : "表单向导"
+      },
+      "CHART" : "图表",
+      "pages" : {
+        "PAGES" : "常用页面",
+        "PROFILE" : "简介",
+        "POST" : "邮件",
+        "SEARCH" : "搜索",
+        "INVOICE" : "发票联",
+        "LOCK_SCREEN" : "锁屏",
+        "SIGNIN" : "登录",
+        "SIGNUP" : "注册",
+        "FORGOT_PASSWORD" : "忘记密码",
+        "404" : "404"
+      }
+    },
+    "your_stuff" : {
+      "YOUR_STUFF": "个人中心",
+      "PROFILE" : "个人简介",
+      "DOCUMENTS" : "文档"
+    }
+  },
+  "MILESTONE" : "里程碑",
+  "RELEASE" : "发行"
+}
+
+}
